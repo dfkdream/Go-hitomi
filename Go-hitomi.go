f60dcbf44fe51ce7dda8e726738b23c97056f08b
dfkdream <dfkdream@gmail.com>
2017-10-19 16:54:14 +0900

    seperate codes into packages

--- Go-hitomi.go
+++ Go-hitomi.go
@@ -2,125 +2,20 @@ package main
 
 import(
 	"fmt"
-	"net/http"
-	"io/ioutil"
 	"os"
-	"encoding/json"
-	"bytes"
 	"runtime"
 	"sync"
 	"flag"
 	"archive/zip"
+	"io/ioutil"
+
+	"github.com/dfkdream/Go-hitomi/downloader"
 )
-
-type ImageInfo struct{
-	Width uint `json:"width"`
-	Name string `json:"name"`
-	Height uint `json:"height"`
-}
-
-func GetImageNames(GalleryID string) []string{
-	fmt.Println("starting downloading")
-	resp,_:=http.Get("https://hitomi.la/galleries/"+GalleryID+".js")
-	defer resp.Body.Close()
-	body,_:=ioutil.ReadAll(resp.Body)
-	body=bytes.Replace(body,[]byte("var galleryinfo = "),[]byte(""),-1)
-	fmt.Println("replaced")
-	var ImageInfo []ImageInfo
-	fmt.Println("starting parsing")
-	json.Unmarshal(body,&ImageInfo)
-	fmt.Println("parsing finished")
-	var ImageNames []string
-	for _,Info := range ImageInfo{
-		ImageNames=append(ImageNames,Info.Name)
-	}
-
-	return ImageNames
-}
-
-func httpsvr(){
-	http.Handle("/",http.StripPrefix("/",http.FileServer(http.Dir("."))))
-
-	http.ListenAndServe(":80",nil)
-}
 
 var Gallery_ID=flag.String("Gallery_ID","","Hitomi.la Gallery ID")
 var Gallery_Name=flag.String("Gallery_Name","","Hitomi.la Gallery name")
 var Do_Compression=flag.Bool("Do_Compression",true,"Compress downloaded files if true")
 var HTTPSvr=flag.Bool("HTTPSvr",false,"Start HTTP Server")
-var mutex=new(sync.Mutex)
-
-func DownloadAndSave(id string, name string, comp bool){
-	var archiveFile *os.File
-	var zipWriter *zip.Writer
-	if comp{
-		//init zip archiver
-		archiveFile,err:=os.OpenFile(
-			name+".zip",
-			os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
-			os.FileMode(0644))
-		if err!=nil{
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		zipWriter=zip.NewWriter(archiveFile)
-	}else{
-		os.Mkdir(name,0777)
-	}
-	ImageNames:=GetImageNames(id)
-	fmt.Println(ImageNames)
-
-	wg:=new(sync.WaitGroup)
-
-	buff:=make(chan string,5)
-
-	for i:=0;i<5;i++{
-		ImageNames=append(ImageNames,"end")
-		wg.Add(1)
-		go func(workerID int){
-			for{
-				Imagename:=<-buff
-				if Imagename=="end"{
-					wg.Done()
-					return
-				}
-				data,_:=http.Get("https://a.hitomi.la/galleries/"+id+"/"+Imagename)
-				defer data.Body.Close()
-				img,err:=ioutil.ReadAll(data.Body)
-				if comp{
-					mutex.Lock()
-					f,err:=zipWriter.Create(Imagename)
-					if err!=nil{
-						fmt.Println(err)
-					}
-					_,err=f.Write(img)
-					if err==nil{
-						fmt.Println("[worker",workerID,"] downloaded",Imagename)
-					}else{
-						fmt.Println(err)
-					}
-					mutex.Unlock()
-				}else{
-					err=ioutil.WriteFile(name+"/"+Imagename,img,os.FileMode(0644))
-					if err==nil{
-						fmt.Println("[worker",workerID,"] downloaded",Imagename)
-					}else{
-						fmt.Println(err)
-					}
-				}		
-			}
-		}(i)
-	}
-	for _,imagename := range ImageNames{
-		buff<-imagename
-	}
-	wg.Wait()
-
-	if *Do_Compression{
-		zipWriter.Close()
-		archiveFile.Close()
-	}
-}
 
 func init(){
 	flag.StringVar(Gallery_ID,"i","","Hitomi.la Gallery ID")
@@ -152,10 +47,89 @@ func main() {
 	fmt.Println("Compression :",*Do_Compression)
 	fmt.Println("Start HTTP Server :",*HTTPSvr)
 
-	DownloadAndSave(*Gallery_ID,*Gallery_Name,*Do_Compression)
+	fmt.Println("fetching image list")
+	img_lst:=downloader.GetImageNamesFromID(*Gallery_ID)
+	num_lst:=len(img_lst)
+	fmt.Println("fetched",num_lst,"images")
+
+	var archiveFile *os.File
+	var zipWriter *zip.Writer
+
+	if *Do_Compression{
+		//init zip archiver
+		archiveFile,err:=os.OpenFile(
+			*Gallery_Name+".zip",
+			os.O_CREATE|os.O_WRONLY|os.O_TRUNC,
+			os.FileMode(0644))
+		if err!=nil{
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		zipWriter=zip.NewWriter(archiveFile)
+	}else{
+		os.Mkdir(*Gallery_Name,0777)
+	}
+
+	ctrl:=make(chan struct{})
+	jobs:=make(chan string)
+	out:=make(chan downloader.Result)
+
+	var wg sync.WaitGroup
+	NumWorkers:=10
+	wg.Add(NumWorkers)
+
+	for i:=0;i<NumWorkers;i++{
+		go func(n int){
+			downloader.DownloadWorker(n,*Gallery_ID,ctrl,jobs,out)
+			wg.Done()
+		}(i)
+	}
+
+	go func(){
+		wg.Wait()
+		close(out)
+	}()
+
+	go func(){
+		for _,work:=range img_lst{
+			jobs <- work
+		}
+		close(jobs)
+	}()
+
+	count:=0
+	for r:=range out{
+		count++
+
+		if *Do_Compression{
+			f,err:=zipWriter.Create(r.ImgName)
+			if err!=nil{
+				fmt.Println(err)
+			}
+			_,err=f.Write(r.Image)
+			if err!=nil{
+				fmt.Println(err)
+			}
+		}else{
+			err:=ioutil.WriteFile(*Gallery_Name+"/"+r.ImgName,r.Image,os.FileMode(0644))
+			if err!=nil{
+				fmt.Println(err)
+			}
+		}
+		fmt.Printf("[worker %d] downloaded %s\n",r.WK_ID,r.ImgName)
+
+		if count==num_lst{
+			close(ctrl)
+		}
+	}
+
+	if *Do_Compression{
+		zipWriter.Close()
+		archiveFile.Close()
+	}
 
 	if *HTTPSvr==true{
 		fmt.Println("HTTP Server started. Press Ctrl+C to exit")
-		httpsvr()
+		downloader.LnsCurrentDirectory()
 	}
 }
